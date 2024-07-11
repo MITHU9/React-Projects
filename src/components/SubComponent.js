06858b1e0be8b85bcc8e157663918bb5bf82a425
Shahariar Mithu <kmmithu2015@gmail.com>
2024-07-11 17:56:46 +0600

    add all

---
+++ src/components/SubComponent.js
@@ -0,0 +1,9 @@
+import "./SubComponent.css";
+function SubComponent() {
+  return (
+    <div className="bgColor">
+      <p>This is a subcomponent.</p>
+    </div>
+  );
+}
+export default SubComponent;
