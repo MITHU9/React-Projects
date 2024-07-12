8c4c430768f093cae34f2a87dfd08205dc57bf55
Shahariar Mithu <kmmithu2015@gmail.com>
2024-07-12 12:48:19 +0600

    file management and props practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React from "react";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+const App = () => {
   let expenses = [
     {
       id: 1,
@@ -29,29 +30,10 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's Get Started!</h2>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-      ></ExpenseItem>
+      <h2 className="center">Let's Get Started!</h2>
+      <Expenses item={expenses} />
     </div>
   );
-}
+};
 
 export default App;
